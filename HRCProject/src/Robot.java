503d6e969f4fb5bdb1ec07a822ffd827709131ec
KnutP <apknut@gmail.com>
2020-10-19 15:32:00 -0400

    Finished several parts of the milestone.
    Finished KeywordSearch, RequestForClarification, and all of Parsing
    except for negation.

--- HRCProject/src/Robot.java
+++ HRCProject/src/Robot.java
@@ -1,5 +1,6 @@
 import java.util.List;
 import java.util.Properties;
+import java.util.Random;
 import java.util.Scanner;
 
 import edu.stanford.nlp.ling.CoreAnnotations;
@@ -13,241 +14,270 @@ import edu.stanford.nlp.util.CoreMap;
 import edu.stanford.nlp.util.Pair;
 
 /**
-	Represents an intelligent agent moving through a particular room.	
-	The robot only has one sensor - the ability to get the status of any  
-	tile in the environment through the command env.getTileStatus(row, col).
-	@author Adam Gaweda, Michael Wollowski
-*/
+ * Represents an intelligent agent moving through a particular room. The robot
+ * only has one sensor - the ability to get the status of any tile in the
+ * environment through the command env.getTileStatus(row, col).
+ * 
+ * @author Adam Gaweda, Michael Wollowski
+ */
 
 public class Robot {
 	private Environment env;
 	private int posRow;
 	private int posCol;
 	private Action lastAction;
-	private String name;
-	private String[] acknowlegements = {
-			"Got it.",
-			"Will do.",
-			"Yep.",
-			"Understood.",
-			"Roger that."
-	};
-	private String[] actionPrepends = {
-			"I think you want me to ",
-			"I am going to ",
-			"",
-			"",
-			""
-	};
-	private String[] clarification = {
+	private String robotName;
+	private String[] acknowlegement = { "Got it. ", "Will do. ", "Yep. ", "Understood. ", "Roger that. " };
+	private String[] actionPrepends = { 
+			"I think you want me to ", 
+			"I am going to ", 
+			"Orders are to ", 
+			"Cool, I'll ", 
+			"Sounds good. I'll " };
+	private String[] clarification = { 
 			"Sorry, what do you want me to do?",
-			"I didn't quite get that. Please try again.",
-			"What was that?",
-			"Unable to parse input.",
-			"I'm confused. What do you want me to do?",
-			"I don't understand. Please try again.",
-			"Bruh, what?",
-			"",
-			"",
-			""
-	};
+			"I didn't quite get that. Please try again.", "What was that?", "Unable to parse input.",
+			"I'm confused. What do you want me to do?", "I don't understand. Please try again.", "Bruh, what?",
+			"I don't know what to do.", "Please clarify.", "I'm not sure what you mean." };
 
 	private Properties props;
 	private StanfordCoreNLP pipeline;
-	
-	private Scanner sc;
-	
-	/**
-	    Initializes a Robot on a specific tile in the environment. 
-	*/
 
-	
-	public Robot (Environment env, int posRow, int posCol) {
+	private Scanner sc;
+
+	/**
+	 * Initializes a Robot on a specific tile in the environment.
+	 */
+
+	public Robot(Environment env, int posRow, int posCol) {
 		this.env = env;
 		this.posRow = posRow;
 		this.posCol = posCol;
 		this.lastAction = Action.DO_NOTHING;
-		this.name = null;
-		
-	    props = new Properties();
-	    props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse");
-	    pipeline = new StanfordCoreNLP(props);
+		this.robotName = null;
 
+		props = new Properties();
+		props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse");
+		pipeline = new StanfordCoreNLP(props);
 
 	}
-	
-	public int getPosRow() { return posRow; }
-	public int getPosCol() { return posCol; }
-	public void incPosRow() { posRow++; }
-	public void decPosRow() { posRow--; }
-	public void incPosCol() { posCol++; }
-    public void decPosCol() { posCol--; }
-	
+
+	public int getPosRow() {
+		return posRow;
+	}
+
+	public int getPosCol() {
+		return posCol;
+	}
+
+	public void incPosRow() {
+		posRow++;
+	}
+
+	public void decPosRow() {
+		posRow--;
+	}
+
+	public void incPosCol() {
+		posCol++;
+	}
+
+	public void decPosCol() {
+		posCol--;
+	}
+
 	/**
-	   Returns the next action to be taken by the robot. A support function 
-	   that processes the path LinkedList that has been populates by the
-	   search functions.
-	*/
-	public Action getAction () {
-	    Annotation annotation;
-	    System.out.print("> ");
-	    sc = new Scanner(System.in); 
-        String name = sc.nextLine(); 
-//	    System.out.println("got: " + name);
-        annotation = new Annotation(name);
-	    pipeline.annotate(annotation);
-	    List<CoreMap> sentences = annotation.get(CoreAnnotations.SentencesAnnotation.class);
-	    if (sentences != null && ! sentences.isEmpty()) {
-	      CoreMap sentence = sentences.get(0);
-	      SemanticGraph graph = sentence.get(SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation.class);
-	      //DONE: remove prettyPrint() and use the SemanticGraph to determine the action to be executed by the robot.
-	      //graph.prettyPrint();
-	      
-	      
-	   // get root of parse graph
-          IndexedWord root = graph.getFirstRoot();
-          // type of root
-          String type = root.tag();
-          switch (type) {
-	          case "VB": return processVerbPhrase(graph, root);
-	          case "JJ": return processAdjectivePhrase(graph, root);
-	          case "NN": return processNounPhrase(graph, root);
-	          default: return processOtherPhrase(graph, root);
-          	}
-	      
-	    }
-	    
-	    this.lastAction = Action.DO_NOTHING;
-	    return Action.DO_NOTHING;
-	}
-    
-	
-	private Action getActionFromWord(String word) {
+	 * Returns the next action to be taken by the robot. A support function that
+	 * processes the path LinkedList that has been populates by the search
+	 * functions.
+	 */
+	public Action getAction() {
+		Annotation annotation;
+		System.out.print("> ");
+		sc = new Scanner(System.in);
+		String name = sc.nextLine();
+		// System.out.println("got: " + name);
+		annotation = new Annotation(name);
+		pipeline.annotate(annotation);
+		List<CoreMap> sentences = annotation.get(CoreAnnotations.SentencesAnnotation.class);
+		if (sentences != null && !sentences.isEmpty()) {
+			CoreMap sentence = sentences.get(0);
+			SemanticGraph graph = sentence
+					.get(SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation.class);
+			// DONE: remove prettyPrint() and use the SemanticGraph to determine
+			// the action to be executed by the robot.
+			// graph.prettyPrint();
+
+			// get root of parse graph
+			IndexedWord root = graph.getFirstRoot();
+			// type of root
+			String type = root.tag();
+			switch (type) {
+			case "VB":
+				return processVerbPhrase(graph, root);
+			case "JJ":
+				return processAdjectivePhrase(graph, root);
+			case "NN":
+				return processNounPhrase(graph, root);
+			default:
+				return processOtherPhrase(graph, root);
+			}
+
+		}
+
+		this.lastAction = Action.DO_NOTHING;
 		return Action.DO_NOTHING;
 	}
-	
-    public Action processVerbPhrase(SemanticGraph dependencies, IndexedWord root){
-        System.out.println("Action: " + root.toString());
-        
-    	if(root.originalText().equalsIgnoreCase("clean")) {
-    		System.out.println("cleaning (Verb Phrase)");
-    		this.lastAction = Action.CLEAN;
-    		return Action.CLEAN;
-    	}
-    	
-    	List<Pair<GrammaticalRelation,IndexedWord>> s = dependencies.childPairs(root);
-    	System.out.println(s.toString());
-    	
-    	for(Pair<GrammaticalRelation,IndexedWord> p : s) {
-    		IndexedWord w = p.second;
 
-	    	if(w.originalText().equalsIgnoreCase("left")) {
-	    		System.out.println("moving left");
-	    		this.lastAction = Action.MOVE_LEFT;
-	    		return Action.MOVE_LEFT;
-	    	} else if(w.originalText().equalsIgnoreCase("right")) {
-	    		System.out.println("moving right");
-	    		this.lastAction = Action.MOVE_RIGHT;
-	    		return Action.MOVE_RIGHT;
-	    	} else if(w.originalText().equalsIgnoreCase("up")) {
-	    		System.out.println("moving up");
-	    		this.lastAction = Action.MOVE_UP;
-	    		return Action.MOVE_UP;
-	    	} else if(w.originalText().equalsIgnoreCase("down")) {
-	    		System.out.println("moving down");
-	    		this.lastAction = Action.MOVE_DOWN;
-	    		return Action.MOVE_DOWN;
-	    	} else if(w.originalText().equalsIgnoreCase("again")) {
-	    		System.out.println("repeating the last action");
-	    		return this.lastAction;
-	    	} else if(w.originalText().equalsIgnoreCase("more")) {
-	    		System.out.println("repeating the last action");
-	    		return this.lastAction;
-	    	} else if(w.originalText().equalsIgnoreCase("further")) {
-	    		System.out.println("repeating the last action");
-	    		return this.lastAction;
-	    	}
+	private Action keywordSearch(SemanticGraph dependencies, IndexedWord root) {
 
-    	}
-    	
-    	System.out.println("doing nothing (Verb Phrase)");
-    	this.lastAction = Action.DO_NOTHING;
-        return Action.DO_NOTHING;
-    }
-    
-    public Action processAdjectivePhrase(SemanticGraph dependencies, IndexedWord root) {
-    	if(root.originalText().equalsIgnoreCase("clean")) {
-    		System.out.println("cleaning (Adjective phrase)");
-    		this.lastAction = Action.CLEAN;
-    		return Action.CLEAN;
-    	}
-    	
-    	return Action.DO_NOTHING;
-    }
-    
-    public Action processNounPhrase(SemanticGraph dependencies, IndexedWord root){
-        System.out.println("Action: " + root.toString());
-        
-    	if(root.originalText().equalsIgnoreCase("clean")) {
-    		System.out.println("cleaning (Noun Phrase)");
-    		return Action.CLEAN;
-    	}
-    	
-    	List<Pair<GrammaticalRelation,IndexedWord>> s = dependencies.childPairs(root);
-    	System.out.println(s.toString());
-    	
-    	for(Pair<GrammaticalRelation,IndexedWord> p : s) {
-    		IndexedWord w = p.second;
-
-	    	if(w.originalText().equalsIgnoreCase("left")) {
-	    		System.out.println("moving left");
-	    		this.lastAction = Action.MOVE_LEFT;
-	    		return Action.MOVE_LEFT;
-	    	} else if(w.originalText().equalsIgnoreCase("right")) {
-	    		System.out.println("moving right");
-	    		this.lastAction = Action.MOVE_RIGHT;
-	    		return Action.MOVE_RIGHT;
-	    	} else if(w.originalText().equalsIgnoreCase("up")) {
-	    		System.out.println("moving up");
-	    		this.lastAction = Action.MOVE_UP;
-	    		return Action.MOVE_UP;
-	    	} else if(w.originalText().equalsIgnoreCase("down")) {
-	    		System.out.println("moving down");
-	    		this.lastAction = Action.MOVE_DOWN;
-	    		return Action.MOVE_DOWN;
-	    	}
-
-    	}
-    	
-    	System.out.println("doing nothing (Noun Phrase)");
+		List<Pair<GrammaticalRelation, IndexedWord>> s = dependencies.childPairs(root);
+		
+		String word = root.originalText();
+		if (word.equalsIgnoreCase("up") || word.equalsIgnoreCase("down")
+				|| word.equalsIgnoreCase("left") || word.equalsIgnoreCase("right")
+				|| word.equalsIgnoreCase("clean") || word.equalsIgnoreCase("again") 
+				|| word.equalsIgnoreCase("more") || word.equalsIgnoreCase("further")
+				|| word.equalsIgnoreCase("repeat")) {
+			return getActionFromWord(word);
+		}
+		
+		for (Pair<GrammaticalRelation, IndexedWord> p : s) {
+			IndexedWord w = p.second;
+			word = w.originalText();
+			
+			if (word.equalsIgnoreCase("up") || word.equalsIgnoreCase("down")
+					|| word.equalsIgnoreCase("left") || word.equalsIgnoreCase("right")
+					|| word.equalsIgnoreCase("clean") || word.equalsIgnoreCase("again") 
+					|| word.equalsIgnoreCase("more") || word.equalsIgnoreCase("further")
+					|| word.equalsIgnoreCase("repeat")) {
+				return getActionFromWord(word);
+			}
+		}
+		
+		System.out.println(getRandom(this.clarification));
 		this.lastAction = Action.DO_NOTHING;
-        return Action.DO_NOTHING;
-    }
-    
-    public static Action processOtherPhrase(SemanticGraph dependencies, IndexedWord root){
-        System.out.println("Action: " + root.toString());
-        
-    	if(root.originalText().equalsIgnoreCase("clean")) {
-    		System.out.println("cleaning (Noun Phrase)");
-    		return Action.CLEAN;
-    	} else if(root.originalText().equalsIgnoreCase("left")) {
-    		System.out.println("moving left");
-    		return Action.MOVE_LEFT;
-    	} else if(root.originalText().equalsIgnoreCase("right")) {
-    		System.out.println("moving right");
-    		return Action.MOVE_RIGHT;
-    	} else if(root.originalText().equalsIgnoreCase("up")) {
-    		System.out.println("moving up");
-    		return Action.MOVE_UP;
-    	} else if(root.originalText().equalsIgnoreCase("down")) {
-    		System.out.println("moving down");
-    		return Action.MOVE_DOWN;
-    	}
-    	
-    	
-    	System.out.println("doing nothing " + root.tag().toString());
-        return Action.DO_NOTHING;
-    }
+		return Action.DO_NOTHING;
+	}
 
+	private static String getRandom(String[] array) {
+		int rnd = new Random().nextInt(array.length);
+		return array[rnd];
+	}
+
+	private Action getActionFromWord(String word) {
+
+		if (word.equalsIgnoreCase("left")) {
+			System.out.print(getRandom(this.acknowlegement));
+			System.out.print(getRandom(this.actionPrepends));
+			System.out.println("move left.");
+			this.lastAction = Action.MOVE_LEFT;
+			return Action.MOVE_LEFT;
+		} else if (word.equalsIgnoreCase("clean")) {
+			System.out.print(getRandom(this.acknowlegement));
+			System.out.print(getRandom(this.actionPrepends));
+			System.out.println("clean the tile.");
+			this.lastAction = Action.CLEAN;
+			return Action.CLEAN;
+		} else if (word.equalsIgnoreCase("right")) {
+			System.out.print(getRandom(this.acknowlegement));
+			System.out.print(getRandom(this.actionPrepends));
+			System.out.println("move right.");
+			this.lastAction = Action.MOVE_RIGHT;
+			return Action.MOVE_RIGHT;
+		} else if (word.equalsIgnoreCase("up")) {
+			System.out.print(getRandom(this.acknowlegement));
+			System.out.print(getRandom(this.actionPrepends));
+			System.out.println("move up.");
+			this.lastAction = Action.MOVE_UP;
+			return Action.MOVE_UP;
+		} else if (word.equalsIgnoreCase("down")) {
+			System.out.print(getRandom(this.acknowlegement));
+			System.out.print(getRandom(this.actionPrepends));
+			System.out.println("move down.");
+			this.lastAction = Action.MOVE_DOWN;
+			return Action.MOVE_DOWN;
+		} else if (word.equalsIgnoreCase("again") || word.equalsIgnoreCase("more") || word.equalsIgnoreCase("further")
+				|| word.equalsIgnoreCase("repeat")) {
+			System.out.print(getRandom(this.acknowlegement));
+			System.out.print(getRandom(this.actionPrepends));
+			System.out.println("repeat the last action.");
+			return this.lastAction;
+		}
+
+		System.out.println(getRandom(this.clarification));
+		this.lastAction = Action.DO_NOTHING;
+		return Action.DO_NOTHING;
+	}
+
+	public Action processVerbPhrase(SemanticGraph dependencies, IndexedWord root) {
+		System.out.println("Action: " + root.toString());
+
+		if (root.originalText().equalsIgnoreCase("clean")) {
+			System.out.println("cleaning (Verb Phrase)");
+			this.lastAction = Action.CLEAN;
+			return Action.CLEAN;
+		}
+
+		List<Pair<GrammaticalRelation, IndexedWord>> s = dependencies.childPairs(root);
+		System.out.println(s.toString());
+
+		for (Pair<GrammaticalRelation, IndexedWord> p : s) {
+			IndexedWord w = p.second;
+			return getActionFromWord(w.originalText());
+		}
+
+		System.out.println("doing nothing (Verb Phrase)");
+		this.lastAction = Action.DO_NOTHING;
+		return Action.DO_NOTHING;
+	}
+
+	public Action processAdjectivePhrase(SemanticGraph dependencies, IndexedWord root) {
+		if (root.originalText().equalsIgnoreCase("clean")) {
+			System.out.print(getRandom(this.acknowlegement));
+			System.out.println("Cleaning.");
+			this.lastAction = Action.CLEAN;
+			return Action.CLEAN;
+		}
+
+		System.out.println(getRandom(this.clarification));
+		this.lastAction = Action.DO_NOTHING;
+		return Action.DO_NOTHING;
+	}
+
+	public Action processNounPhrase(SemanticGraph dependencies, IndexedWord root) {
+		System.out.println("Action: " + root.toString());
+
+		if (root.originalText().equalsIgnoreCase("clean")) {
+			System.out.print(getRandom(this.acknowlegement));
+			System.out.println("Cleaning.");
+			this.lastAction = Action.CLEAN;
+			return Action.CLEAN;
+		} else if (root.originalText().equalsIgnoreCase("repeat")) {
+			System.out.print(getRandom(this.acknowlegement));
+			System.out.println("Repeating the last action.");
+			return this.lastAction;
+		}
+
+		List<Pair<GrammaticalRelation, IndexedWord>> s = dependencies.childPairs(root);
+		System.out.println(s.toString());
+
+		for (Pair<GrammaticalRelation, IndexedWord> p : s) {
+			IndexedWord w = p.second;
+
+			return getActionFromWord(w.originalText());
+
+		}
+
+		System.out.println(getRandom(this.clarification));
+		this.lastAction = Action.DO_NOTHING;
+		return Action.DO_NOTHING;
+	}
+
+	public Action processOtherPhrase(SemanticGraph dependencies, IndexedWord root) {
+		System.out.println("Action: " + root.toString());
+		
+		return keywordSearch(dependencies, root);
+	}
 
 }
